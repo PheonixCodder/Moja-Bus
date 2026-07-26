3ed4f115ef7f6d5c956429364378e6d4e0e54753
Ubaidullah Ismail <abdulahadismail.dev@gmail.com>
2026-07-26 10:10:07 -0700

    Update phone-input.tsx

--- packages/ui/src/components/ui/phone-input.tsx
+++ packages/ui/src/components/ui/phone-input.tsx
@@ -104,6 +104,7 @@ const InputComponent = React.forwardRef<
   React.ComponentProps<"input">
 >(({ className, ...props }, ref) => (
   <Input
+    maxLength={10}
     className={cn("rounded-e-lg rounded-s-none", className)}
     {...props}
     ref={ref}
